d8d9092be26d930bb53f5728a1e917fd646d869c
talsky1 <60748962+talsky1@users.noreply.github.com>
2020-02-07 10:50:56 +0200

    Functions

---
+++ d3.functions.py
@@ -0,0 +1,10 @@
+
+
+def say_hello(person1 , person2):
+    print("Hello" , person1 + ", how are you doing?","your",  person2 , "is waiting for you")
+    
+def far2celcius(fahr):
+    celcius = (5*(fahr - 32)) / 9
+    return celcius
+
+say_hello("Tal", "beer")
